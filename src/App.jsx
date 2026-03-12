a8e325d7e5c0c297319ce3435058e765f3b66445
Thiago Yashimura Torres <thiagoyashimura03@gmail.com>
2026-03-12 12:31:58 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './index.css'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-blue-400 p-3 text-white'>App</div>
+  )
+}
+
+export default App
